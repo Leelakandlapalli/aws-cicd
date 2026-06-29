32e91e815a80860f2a63a9f33e3996c1fb0f750c
Leelakandlapalli <86658229+Leelakandlapalli@users.noreply.github.com>
2026-06-29 14:01:09 +0530

    Update Course.java

--- src/main/java/com/javatechie/dto/Course.java
+++ src/main/java/com/javatechie/dto/Course.java
@@ -9,9 +9,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Course {
    private int id;
-    public int getId() {
+   
+   public int getId() {
         return id;
+   }
+   public void setId(int id) {
+      this.id = id;
+   }
     private String name;
     private double price;
-    }
+    
 }
